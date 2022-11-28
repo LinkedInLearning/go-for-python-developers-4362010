9f3fa8e6fe90573644472d864067059246f0a1d3
Miki Tebeka <miki@353solutions.com>
2022-11-28 18:33:47 +0200

    doctest

--- Ch06/06_01/rtb.py
+++ Ch06/06_01/rtb.py
@@ -1,4 +1,9 @@
 def second_best(prices):
+    """Return second best price.
+
+    >>> second_best([2, 1, 3])
+    2
+    """
     if not prices:
         raise ValueError('second_best on empty prices')
 
